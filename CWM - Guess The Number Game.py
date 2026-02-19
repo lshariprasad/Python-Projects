fe414dd8e02efb9e0727d931b30c3637c2f673b2
HARI PRASAD L S <06hariumaraja@gmail.com>
2026-02-19 23:14:30 +0530

    Add comment about game purpose for beginners
    Added a comment to indicate the game's purpose for beginners.

--- CWM - Guess The Number Game.py
+++ CWM - Guess The Number Game.py
@@ -6,6 +6,7 @@ guess_limit = 3
 while guess_count < guess_limit :
     guess = int(input("Enter Your Guess Number : "))
     guess_count +=1 
+    
     if guess == secrect_number:
         print (" You Won The Game ")
         break
@@ -14,3 +15,5 @@ else:
     print(" Try Next Time... ")
 
 
+
+# This Game is for build fir beginners
